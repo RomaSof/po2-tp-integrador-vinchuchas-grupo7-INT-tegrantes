1669be525106dbc50135ca904b61d2902c00399b
Ivan Leguizamon <ivanleguizamon939@gmail.com>
2025-06-15 01:34:06 -0300

    Modifica CriterioFechaUltimaVotacion: Corregido el metodo getFechaUltimaVotacion() pasado a Muestra (En el coverage no esta siendo cubierto - revisar)

--- src/criterio/CriterioFechaUltimaVotacion.java
+++ src/criterio/CriterioFechaUltimaVotacion.java
@@ -24,36 +24,9 @@ public class CriterioFechaUltimaVotacion implements CriterioBusqueda{
 		return this.fechaAFiltrar;
 	}
 	
-	// Adaptado con Date correctamente!
-	// Este metodo deberia estar en Muestra porque creo que no esta bien que esta clase trabaje sobre una muestra.
-	
-	// El propósito del método es obtener la fecha de la opinión más reciente asociada a una muestra.
-	// Primero verifica si la lista de opiniones está vacía; si lo está, retorna null.
-	// Luego inicializa la variable `fechaMasReciente` con la fecha de la primera opinión.
-	// Después recorre cada opinión y compara su fecha con la fecha actualmente almacenada en `fechaMasReciente`.
-	// Si encuentra una fecha más reciente, la actualiza.
-	// Finalmente, devuelve la fecha más actual.
-	public Date getFechaUltimaVotacion(Muestra muestra) {
-	    List<Opinion> opiniones = muestra.getHistorialDeOpiniones();
-	    
-	    if (opiniones.isEmpty()) {
-	        return null;
-	    }
-	    
-	    Date fechaMasReciente = opiniones.get(0).getFechaOpinion();
-	    
-	    for (Opinion opinion : opiniones) { 	
-	    	Date fechaActual = opinion.getFechaOpinion();
-	    	if(fechaActual.after(fechaMasReciente)) {
-	    		fechaMasReciente = fechaActual;
-	    	}
-	    }
-	    return fechaMasReciente;
-	}
-
 	@Override
 	public boolean cumple(Muestra muestra) {
-		Date fechaUltimaVotacion = this.getFechaUltimaVotacion(muestra);
+		Date fechaUltimaVotacion = muestra.getFechaUltimaVotacion();
 		return this.getFiltroFecha().comparar(fechaUltimaVotacion, this.getFechaAFiltrar());
 	}
 }
